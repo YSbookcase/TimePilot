426dae92580cf5bdb904fdda442559f92080e88f
YSbookcase <76533032+YSbookcase@users.noreply.github.com>
2026-05-26 02:04:36 +0900

    feat: show Windows events on timeline

---
+++ TimePilot.WinForms/KYS24/TimelineSystemEventFilter.cs
@@ -0,0 +1,11 @@
+namespace TimePilot.WinForms.KYS24
+{
+    internal enum TimelineSystemEventFilter
+    {
+        All = 0,
+        Lock = 1,
+        Power = 2,
+        Shutdown = 3,
+        TimePilot = 4
+    }
+}
