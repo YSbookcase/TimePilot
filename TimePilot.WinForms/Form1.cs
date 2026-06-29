5ebcd2408fefa74491e3608e875bce36e1153e7d
YSbookcase <76533032+YSbookcase@users.noreply.github.com>
2026-06-29 10:14:35 +0900

    refactor: complete table state separation

--- TimePilot.WinForms/Form1.cs
+++ TimePilot.WinForms/Form1.cs
@@ -201,59 +201,6 @@ namespace TimePilot.WinForms
             Shown += OnShown;
         }
 
-        private void ApplySavedTableSortState()
-        {
-            usageSortProperty = GridSortPropertyResolver.NormalizeUsageSortProperty(settings.UsageSortProperty);
-            usageSortOrder = GridSortOrderHelper.FromSavedDescending(settings.UsageSortDescending, SortOrder.Descending);
-            dailyUsageTrendSortProperty = GridSortPropertyResolver.NormalizeDailyUsageTrendSortProperty(settings.DailyUsageTrendSortProperty);
-            dailyUsageTrendSortOrder = GridSortOrderHelper.FromSavedDescending(settings.DailyUsageTrendSortDescending, SortOrder.Descending);
-            timelineSortProperty = GridSortPropertyResolver.NormalizeTimelineSortProperty(settings.TimelineSortProperty);
-            timelineSortOrder = GridSortOrderHelper.FromSavedDescending(settings.TimelineSortDescending, SortOrder.Descending);
-            runtimeSortProperty = GridSortPropertyResolver.NormalizeRuntimeSortProperty(settings.RuntimeSortProperty);
-            runtimeSortOrder = GridSortOrderHelper.FromSavedDescending(settings.RuntimeSortDescending, SortOrder.Descending);
-            runtimeSegmentSortProperty = GridSortPropertyResolver.NormalizeRuntimeSegmentSortProperty(settings.RuntimeSegmentSortProperty);
-            runtimeSegmentSortOrder = GridSortOrderHelper.FromSavedDescending(settings.RuntimeSegmentSortDescending, SortOrder.Descending);
-        }
-
-        private void SaveTableSortState()
-        {
-            settings.UsageSortProperty = usageSortProperty;
-            settings.UsageSortDescending = usageSortOrder == SortOrder.Descending;
-            settings.DailyUsageTrendSortProperty = dailyUsageTrendSortProperty;
-            settings.DailyUsageTrendSortDescending = dailyUsageTrendSortOrder == SortOrder.Descending;
-            settings.TimelineSortProperty = timelineSortProperty;
-            settings.TimelineSortDescending = timelineSortOrder == SortOrder.Descending;
-            settings.RuntimeSortProperty = runtimeSortProperty;
-            settings.RuntimeSortDescending = runtimeSortOrder == SortOrder.Descending;
-            settings.RuntimeSegmentSortProperty = runtimeSegmentSortProperty;
-            settings.RuntimeSegmentSortDescending = runtimeSegmentSortOrder == SortOrder.Descending;
-            settings.Save();
-        }
-
-        private void ResetTableSortState()
-        {
-            settings.ResetTableSortStates();
-            ApplySavedTableSortState();
-            ApplyTableColumnLayouts(defaultTableColumnLayouts);
-            UpdateSortGlyphs();
-            RefreshViews(DateTimeOffset.UtcNow);
-            SetStatusText(GetTableSortResetStatusText());
-        }
-
-        private string GetResetTableSortMenuText()
-        {
-            return settings.UiLanguage == UiLanguage.English
-                ? "Reset table sorting"
-                : "화면 정렬 초기화";
-        }
-
-        private string GetTableSortResetStatusText()
-        {
-            return settings.UiLanguage == UiLanguage.English
-                ? "Table sorting has been reset to defaults."
-                : "화면 정렬을 기본값으로 되돌렸습니다.";
-        }
-
         private string GetAppCategoryManagementMenuText()
         {
             return settings.UiLanguage == UiLanguage.English
@@ -1015,15 +962,6 @@ namespace TimePilot.WinForms
             RefreshViews(DateTimeOffset.UtcNow);
         }
 
-        private void UpdateSortGlyphs()
-        {
-            GridSortGlyphUpdater.UpdateGlyphs(usageGrid, GridSortPropertyResolver.GetUsageSortPropertyName, usageSortProperty, usageSortOrder);
-            GridSortGlyphUpdater.UpdateGlyphs(dailyUsageTrendGrid, GridSortPropertyResolver.GetDailyUsageTrendSortPropertyName, dailyUsageTrendSortProperty, dailyUsageTrendSortOrder);
-            GridSortGlyphUpdater.UpdateGlyphs(timelineGrid, GridSortPropertyResolver.GetTimelineSortPropertyName, timelineSortProperty, timelineSortOrder);
-            GridSortGlyphUpdater.UpdateGlyphs(runtimeGrid, GridSortPropertyResolver.GetRuntimeSortPropertyName, runtimeSortProperty, runtimeSortOrder);
-            GridSortGlyphUpdater.UpdateGlyphs(runtimeSegmentsGrid, GridSortPropertyResolver.GetRuntimeSegmentSortPropertyName, runtimeSegmentSortProperty, runtimeSegmentSortOrder);
-        }
-
         private void ConfigureHeaderToolTip()
         {
             headerToolTipLabel.AutoSize = false;
@@ -1112,11 +1050,6 @@ namespace TimePilot.WinForms
         private void OnPreferencesMenuItemClick(object? sender, EventArgs e)
         {
             ShowPreferencesDialog();
-        }
-
-        private void OnResetTableSortMenuItemClick(object? sender, EventArgs e)
-        {
-            ResetTableSortState();
         }
 
         private void OnAppCategoryManagementMenuItemClick(object? sender, EventArgs e)
